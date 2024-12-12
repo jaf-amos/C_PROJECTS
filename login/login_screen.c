17e0d5a57a2e713bc8dbe93080e1b86e79f972fc
Amos Araunah <araunahamos69@gmail.com>
2024-12-12 07:49:17 +0100

    Adding functions to the code

--- login/login_screen.c
+++ login/login_screen.c
@@ -23,56 +23,63 @@ char getch() {
     return ch;
 }
 
+char *pass_func(void)
+    {
+    // Loop to get character from keyboard input. Hides input by displaying asterisk character
+        char ch;
+        int i = 0;
+        char *pass;
+
+        pass = malloc(20 * sizeof(char));
+        /*if (pass == NULL)
+        {
+            return EXIT_FAILURE;
+        }
+        */
+        while (1)
+        {
+            i = 0;
+            //memset(pass, 0, sizeof(pass));
+
+            while (i < 19)
+            {
+                ch = getch();
+                if (ch == '\n' || ch == '\r')
+                {
+                    pass[i] = '\0';
+                    break;
+                } 
+                else if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9') )
+                {
+                    pass[i++] = ch;
+                    printf("*");
+                }
+                else
+                {
+                    pass[i++] = ch;
+                    printf("*");
+                }
+            }
+
+            if (i > 0 && pass[i] == '\0')
+            {
+                break;
+            }
+        }
+        return pass;
+    }
+
 /**
  * main - entry point of the program
  * Return: 0
 */
 int main(void) {
-    char ch;
-    char pass[20];
-    char saved_pass[20];
-    int i = 0;
     int l;
+    int pass_input = 0;
+    char saved_pass[20];
 
     printf("Enter a password containing only alphanumeric characters!\n");
-    // Loop to get character from keyboard input. Hides input by displaying asterisk character
-    while (1)
-    {
-        i = 0;
-        memset(pass, 0, sizeof(pass));
 
-        while (i < 19)
-        {
-            ch = getch();
-            if (ch == '\n' || ch == '\r')
-            {
-                pass[i] = '\0';
-                break;
-            } 
-            else if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9') )
-            {
-                pass[i++] = ch;
-                printf("*");
-            }
-            else
-            {
-                pass[i++] = ch;
-                printf("*");
-            }
-            /*else
-            {
-                printf("You have entered an invalid character.\n");
-                sleep(1);
-                printf("\033[H\033[J");
-                break;
-            }*/
-        }
-
-        if (i > 0 && pass[i] == '\0')
-        {
-            break;
-        }
-    }
 
     // Retrieves password from file and saves it in saved_pass buffer
 
@@ -96,12 +103,15 @@ int main(void) {
     saved_pass[strcspn(saved_pass, "\n")] = '\0';
     fclose(ptr);
 
+    while (pass_input == 0)
+    {
     // Compares the input password and the saved password
-    l = strcmp (pass, saved_pass);
+    l = strcmp (pass_func(), saved_pass);
     if (l == 0)
     {
         printf("\nLogin successful\n");
-        return EXIT_SUCCESS;
+        pass_input = 1;
+       // return EXIT_SUCCESS;
     }
     else
     {
@@ -109,7 +119,7 @@ int main(void) {
         sleep(1);
         printf("\033[H\033[J");
     }
-        
+    }
     return 0;
 }
 
